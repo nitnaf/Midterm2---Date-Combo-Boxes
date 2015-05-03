e4c324650eaeab6cc2546d215b43891c3d5724c9
Steven Fantin <sfantin@udel.edu>
2015-05-03 09:22:00 -0400

    Commit

--- src/ch/makery/address/view/Controller.java
+++ src/ch/makery/address/view/Controller.java
@@ -1,9 +1,17 @@
 package ch.makery.address.view;
 
+import java.net.URL;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 
-public class Controller {
+public class Controller implements Initializable {
 	
 	@FXML
 	private ComboBox monthBox;
@@ -14,28 +22,112 @@ public class Controller {
 	@FXML
 	private ComboBox yearBox;
 	
-	@FXML
-	private void loadDays(){
+	private void loadMonths(){
+		
+		ObservableList<String> months = 
+			    FXCollections.observableArrayList(
+			        "January",
+			        "February",
+			        "March",
+			        "April",
+			        "May",
+			        "June",
+			        "July",
+			        "August",
+			        "September",
+			        "October",
+			        "November",
+			        "December");
+		
+		monthBox.setItems(months);
+	}
+	
+	private boolean leapYear(){
+		
+		boolean leapYear = ((Integer.parseInt(yearBox.getValue().toString()) % 4 == 0) && (Integer.parseInt(yearBox.getValue().toString()) % 100 != 0) || (Integer.parseInt(yearBox.getValue().toString()) % 400 == 0));
+		return leapYear;
+	}
 
-//		if (monthBox.getValue().toString().equals("February"))
-//		{
-//
-//			if (yearBox.getValue() = !null) {
-//
-//				boolean leapYear = ((yearBox.getValue() % 4 == 0) && (yearBox.getValue() % 100 != 0) || (yearBox.getValue() % 400 == 0));
-//
-//				if (leapYear) { LOAD LIST OF 29 DAYS
-//				
-//				} else { LOAD LIST OF 28 DAYS }
-//
-//			} else { LOAD LIST OF 29 DAYS } -- Just in case they didn't select a year yet.
-//  	}
-//
-//		if (monthBox.getValue().toString() = "January" || "March" || "May" || "July" || "August" || "October" || "December") {
-//
-//				LOAD LIST OF 31 DAYS
-//				
-//			} else { LOAD LIST OF 30 DAYS }
+	@FXML
+	private void loadDays() {
+
+		ObservableList<String> days28 = FXCollections.observableArrayList("1",
+				"2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13",
+				"14", "15", "16", "17", "18", "19", "20", "21", "22", "23",
+				"24", "25", "26", "27", "28");
+
+		ObservableList<String> days29 = FXCollections.observableArrayList("1",
+				"2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13",
+				"14", "15", "16", "17", "18", "19", "20", "21", "22", "23",
+				"24", "25", "26", "27", "28", "29");
+
+		ObservableList<String> days30 = FXCollections.observableArrayList("1",
+				"2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13",
+				"14", "15", "16", "17", "18", "19", "20", "21", "22", "23",
+				"24", "25", "26", "27", "28", "29", "30");
+
+		ObservableList<String> days31 = FXCollections.observableArrayList("1",
+				"2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13",
+				"14", "15", "16", "17", "18", "19", "20", "21", "22", "23",
+				"24", "25", "26", "27", "28", "29", "30", "31");
+
+		if (monthBox.getValue().toString().equals("February")) {
+
+			if (yearBox.getValue().equals(null)) {
+
+				monthBox.setItems(days29);
+
+			} else {
+
+				if (leapYear()) {
+
+					monthBox.setItems(days29);
+
+				} else {
+
+					monthBox.setItems(days28);
+				}
+			}
+
+			if (monthBox.getValue().toString().equals("January")) {
+				monthBox.setItems(days31);
+			}
+			if (monthBox.getValue().toString().equals("March")) {
+				monthBox.setItems(days31);
+			}
+			if (monthBox.getValue().toString().equals("May")) {
+				monthBox.setItems(days31);
+			}
+			if (monthBox.getValue().toString().equals("July")) {
+				monthBox.setItems(days31);
+			}
+			if (monthBox.getValue().toString().equals("August")) {
+				monthBox.setItems(days31);
+			}
+			if (monthBox.getValue().toString().equals("October")) {
+				monthBox.setItems(days31);
+			}
+			if (monthBox.getValue().toString().equals("December")) {
+				monthBox.setItems(days31);
+			} else {
+				monthBox.setItems(days30);
+			}
+
+		}
+	}
+	
+	public void loadYear(){
+	ObservableList<String> years = FXCollections.observableArrayList("2015");
+	yearBox.setItems(years);	
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+		loadYear();
+		loadMonths();
+		//loadDays();
+		
 	}
 	
 	
